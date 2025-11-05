c849b23960313b0451e8616b79f349af27288bc0
Gerry <gerrywiegand@gmail.com>
2025-11-05 10:36:38 -0500

    implemented register class, discount property and setter and add_item

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
+
 class CashRegister:
-  pass
+    def __init__(self, discount=0, total=0, items=None, previous_transactions=None):
+        self.discount = discount
+        self.total = total
+        self.items = items if items is not None else []
+        self.previous_transactions = (
+            previous_transactions if previous_transactions is not None else []
+        )
+
+
+@property
+def discount(self):
+    return self._discount
+
+
+@discount.setter
+def discount(self, value):
+    if isinstance(value, int) and value >= 0:
+        self._discount = value
+    else:
+        raise ValueError("Discount must be a non-negative integer")
+
+
+def add_item(self, title, price, qty):
+    self.total += price * qty
+    self.items.append(title)
+    self.previous_transactions.append({"title": title, "price": price, "qty": qty})
